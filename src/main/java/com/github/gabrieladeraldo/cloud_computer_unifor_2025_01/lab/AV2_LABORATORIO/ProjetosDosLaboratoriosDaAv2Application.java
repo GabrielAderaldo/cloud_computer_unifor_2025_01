55d1eba899abb349770f66e2729955d6a1f8aabc
Gabriel Aderaldo <gabrieladeraldo@Mac-mini-de-Gabriel.local>
2025-10-02 01:53:52 -0300

    [INFRA] - Adicionando arquivos do projeto ao controle de versão

---
+++ src/main/java/com/github/gabrieladeraldo/cloud_computer_unifor_2025_01/lab/AV2_LABORATORIO/ProjetosDosLaboratoriosDaAv2Application.java
@@ -0,0 +1,11 @@
+package com.github.gabrieladeraldo.cloud_computer_unifor_2025_01.lab.AV2_LABORATORIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosDosLaboratoriosDaAv2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosDosLaboratoriosDaAv2Application.class, args);
+	}
+}
